655f374769a250a731b529c1acda573240579fe8
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-15 18:55:44 -0800

    cmmit

--- hf_caching_demo/launch.py
+++ hf_caching_demo/launch.py
@@ -1,7 +1,11 @@
 import argparse
 import gcsfs
 import datasets
-from etils import epath
+import posixpath
+import transformers
+
+SEQLEN = 512
+TEXTCOL = "text"
 
 
 # to make an api key, follow instructions at
@@ -9,32 +13,35 @@ from etils import epath
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--hfds_identifier", type=str, help="HF datasets identifier")
+    parser.add_argument("--hfds_split_name", type=str, help="HF datasets split name")
     parser.add_argument("--gc_project", type=str, help="Google Cloud project")
     parser.add_argument("--gc_secret_key", type=str, help="Google Cloud api key")
     parser.add_argument("--gc_storage_uri", type=str, help="Google Cloud storage path")
     args = parser.parse_args()
-    storage_options = dict(
-        project=args.gc_project,
-        key=args.gc_secret_key,
-    )
+    storage_options = dict(project=args.gc_project, key=args.gc_secret_key)
     fs = gcsfs.GCSFileSystem(**storage_options)  # todo: do we need this line?
+    ds_all_splits = datasets.load_dataset(args.hfds_identifier, streaming=True)
+    tokenizer_ = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
 
-    # maybe cache the built dataset
-    output_dir = (epath.Path(args.gc_storage_uri) / "staging").as_uri()
-    builder = datasets.load_dataset_builder(args.hfds_identifier)
-    builder.download_and_prepare(
-        output_dir=output_dir,
+    def tokenize_fast(examples):
+        return tokenizer_(
+            examples[TEXTCOL],
+            padding="max_length",
+            truncation=True,
+            max_length=SEQLEN,
+        )["input_ids"]
+
+    for s in ds_all_splits.keys():
+        path_s = posixpath.join(args.gc_storage_uri, s),
+        if not fs.exists(path_s):
+            ds = ds_all_splits.get(s)
+            ds = ds.map(tokenize_fast, batch_size=1024)
+            ds.save_to_disk(path_s, storage_options=storage_options)
+
+    return datasets.load_from_disk(
+        dataset_path=posixpath.join(args.gc_storage_uri, args.hfds_split_name),
         storage_options=storage_options,
     )
-    # and maybe process further
-    # todo: this
-    # and maybe save the processed dataset
-    # encoded_dataset.save_to_disk(
-    #     "gcs://my-private-datasets/imdb/train",
-    #     storage_options=storage_options,
-    # )
-
-    # load the processed dataset
 
 
 if __name__ == "__main__":
