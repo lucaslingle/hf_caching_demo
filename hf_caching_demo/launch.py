8db73448ffad90e08c3b3cd0a7924b1b89671c65
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-15 20:27:28 -0800

    commit

--- hf_caching_demo/launch.py
+++ hf_caching_demo/launch.py
@@ -18,8 +18,8 @@ def main():
     parser.add_argument("--gc_secret_key", type=str, help="Google Cloud api key")
     parser.add_argument("--gc_storage_uri", type=str, help="Google Cloud storage path")
     args = parser.parse_args()
-    fs = gcsfs.GCSFileSystem(project=args.gc_project, token=None)
-    storage_options = dict(project=args.gc_project, key=args.gc_secret_key)
+    storage_options = dict(project=args.gc_project)  #, key=args.gc_secret_key)
+    fs = gcsfs.GCSFileSystem(**storage_options)  #, token=None)
     ds_all_splits = datasets.load_dataset(
         args.hfds_identifier,
         cache_dir=posixpath.join(args.gc_storage_uri, "staging"),
