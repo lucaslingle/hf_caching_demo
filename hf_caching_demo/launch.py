ebe229c3d236fb03948700fd7fab78f2fbfb5317
lucaslingle <lucasdaxlingle@gmail.com>
2024-01-16 23:06:40 -0800

    commit

--- hf_caching_demo/launch.py
+++ hf_caching_demo/launch.py
@@ -1,9 +1,9 @@
 # todo: maybe do the token shift inside model to reduce host-to-device io
 
-import argparse
-
-from absl import logging
 import datasets as hfds
+from absl import app
+from absl import flags
+from absl import logging
 
 from dataloader import get_tokenizer, get_dataset
 
@@ -13,24 +13,26 @@ BATCH_SIZE = 8
 SEQLEN = 512
 TEXTCOL = "text"
 
+FLAGS = flags.FLAGS
+flags.DEFINE_string("hfds_identifier", None, "HF datasets identifier")
+flags.DEFINE_string("hfds_split_name", None, "HF datasets split name")
+flags.mark_flags_as_required(["hfds_identifier", "hfds_split_name"])
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--hfds_identifier", type=str, help="HF datasets identifier")
-    parser.add_argument("--hfds_split_name", type=str, help="HF datasets split name")
-    args = parser.parse_args()
+
+def main(argv):
+    del argv
 
     logging.info(f"calling get_tokenizer to get fast tokenizer...")
     tokenizer = get_tokenizer("GPT2TokenizerFast", "gpt2")
 
     logging.info(f"calling get_dataset to get sharded bitwise reproducible dataset...")
     ds = get_dataset(
-        hfds_identifier=args.hfds_identifier,
+        hfds_identifier=FLAGS.hfds_identifier,
         hfds_config=None,
         hfds_datacol=TEXTCOL,
         hfds_buffer_size=1024,
         hftr_tokenizer=tokenizer,
-        split_name=args.hfds_split_name,
+        split_name=FLAGS.hfds_split_name,
         batch_size=BATCH_SIZE,
         sequence_len=SEQLEN,
         step=STEP,
@@ -39,4 +41,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    app.run(main)
